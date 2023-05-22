35c808a8f6f1500afe9db47502e87f3407a4397c
danhNg.8 <105102066+danhNg08101997@users.noreply.github.com>
2023-05-22 22:46:05 +0700

    header component

--- src/app/layouts/layouts-routing.module.ts
+++ src/app/layouts/layouts-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailComponent } from './pages/detail/detail.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { LayoutsComponent } from './layouts.component';
 
 const layoutsRoutes: Routes = [
